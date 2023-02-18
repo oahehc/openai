8b3c90153e4fc75792ca9c5737021ba460115d8c
oahehc <oahehc@gmail.com>
2023-02-18 10:14:25 +0900

    feat: tags for generation

--- pages/text.tsx
+++ pages/text.tsx
@@ -1,9 +1,21 @@
 import { useState } from "react";
-import { Input, List, Button, Space, Alert } from "antd";
+import { Input, List, Button, Space, Alert, Tag } from "antd";
 import { fetcher } from "../utils/fetcher";
 import CopyButton from "../components/CopyButton";
 
 const { TextArea } = Input;
+
+type TagType = "fix-grammar" | "correction" | "native" | "translation";
+// support color: https://ant.design/components/tag
+const TagColorMap: Record<TagType, string> = {
+  "fix-grammar": "magenta",
+  correction: "gold",
+  native: "geekblue",
+  translation: "volcano",
+  // temp: 'orange',
+  // temp: 'cyan',
+  // temp: 'purple',
+};
 
 export default function Page() {
   const [text, setText] = useState("");
@@ -11,65 +23,66 @@ export default function Page() {
   const [error, setError] = useState("");
   const [result, setResult] = useState<any[]>([]);
 
-  function startFetching() {
-    setIsLoading((cur) => cur + 1);
-    setError("");
-  }
-  function finishFetching() {
-    setIsLoading((cur) => cur - 1);
-  }
-
-  async function useCompletion(text) {
+  async function sendRequest({
+    type,
+    text,
+    instruction,
+    tags = [],
+  }: {
+    type: "completion" | "edit";
+    text: string;
+    tags?: TagType[];
+    instruction?: string;
+  }) {
     try {
-      startFetching();
+      setIsLoading((cur) => cur + 1);
+      setError("");
 
       const data = await fetcher({
         method: "POST",
-        path: "/api/completion",
+        path: `/api/${type}`,
         body: {
           text,
+          instruction: type === "edit" && instruction ? instruction : "",
         },
       });
 
-      setResult((cur) => [...cur, ...data.result]);
+      setResult((cur) => [
+        ...cur,
+        ...data.result.map(({ text }) => ({ text, tags })),
+      ]);
     } catch (error) {
       console.error(error);
       setError(error.message);
     } finally {
-      finishFetching();
-    }
-  }
-  async function useEdit(text, instruction) {
-    try {
-      startFetching();
-
-      const data = await fetcher({
-        method: "POST",
-        path: "/api/edit",
-        body: {
-          text,
-          instruction,
-        },
-      });
-
-      setResult((cur) => [...cur, ...data.result]);
-    } catch (error) {
-      console.error(error);
-      setError(error.message);
-    } finally {
-      finishFetching();
+      setIsLoading((cur) => cur - 1);
     }
   }
 
   function getTranslations() {
-    useCompletion(
-      `Translate this into 1. Chinese, 2. Japanese:\n\n${text}\n\n`
-    );
+    sendRequest({
+      type: "completion",
+      text: `Translate this into 1. Chinese, 2. Japanese:\n\n${text}\n\n`,
+      tags: ["translation"],
+    });
   }
   function handelSubmit() {
-    useCompletion(`Correct this to standard English:\n\n${text}`);
-    useCompletion(`To make 「${text}」 close to native speaker, we can say:`);
-    useEdit(text, "Fix the grammar mistakes");
+    sendRequest({
+      type: "completion",
+      text: `Correct this to standard English:\n\n${text}`,
+      tags: ["correction"],
+    });
+    sendRequest({
+      type: "completion",
+      text: `To make 「${text}」 close to native speaker, we can say:`,
+      tags: ["native"],
+    });
+    sendRequest({
+      type: "edit",
+      text,
+      instruction: "Fix the grammar mistakes",
+      tags: ["fix-grammar"],
+    });
   }
 
   function handleEnter(e) {
@@ -108,12 +121,17 @@ export default function Page() {
           }
           bordered
           dataSource={result}
-          renderItem={({ text }) => {
+          renderItem={({ text, tags }) => {
             return (
               text && (
                 <List.Item style={{ display: "flex" }}>
                   <CopyButton content={text} />
                   <p style={{ flex: 1, marginLeft: "8px" }}>{text}</p>
+                  {tags.map((tag) => (
+                    <Tag key={tag} color={TagColorMap[tag]}>
+                      {tag}
+                    </Tag>
+                  ))}
                 </List.Item>
               )
             );
